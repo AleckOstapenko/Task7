c7a8210783e8b03255c9bf86ac7444332ed88bef
Aleksandr <aleck.ostapenko@gmail.com>
2020-05-05 00:22:25 +0300

    Первый вариант отредактированный

--- js/script.js
+++ js/script.js
@@ -1,30 +1,27 @@
 'use strict';
 
-let buttonStart = document.getElementById('start'), 
-	
+let startBtn = document.getElementById('start'), 	
 	budgetValue = document.getElementsByClassName('budget-value')[0],
-	daybudgetValue = document.getElementsByClassName('daybudget-value')[0],
+	dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
 	levelValue = document.getElementsByClassName('level-value')[0],
 	expensesValue = document.getElementsByClassName('expenses-value')[0],
-	optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+	optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
 	incomeValue = document.getElementsByClassName('income-value')[0],
-	monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0],
-	yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+	monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+	yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
 
-	expencesItems = document.getElementsByClassName('expenses-item'),
-
-	buttonExpences = document.getElementsByTagName("button")[0],
-	buttonOptionalExpenses = document.getElementsByTagName("button")[1],
-
-	optionalExpensesItems = document.querySelectorAll('.optionalexpenses-item'),
-
-	chooseIncomeItem = document.querySelector('.choose-income'),
-	savingsCheckbox = document.querySelector(".checksavings"),
-	sumSavingItem = document.querySelector(".choose-sum"),
-	percentSavingItem = document.querySelector(".choose-percent"),
-	yearValue=document.querySelector(".year-value"),
-	monthValue=document.querySelector(".month-value"),
-	dayValue=document.querySelector(".day-value");
+	expensesItem = document.getElementsByClassName('expenses-item'),
+	expensesBtn = document.getElementsByTagName("button")[0],
+	optionalExpensesBtn = document.getElementsByTagName("button")[1],
+	countBtn = document.getElementsByTagName('button')[2],
+	optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+	incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector("#savings"),
+	sumValue = document.querySelector(".choose-sum"),
+	percentValue = document.querySelector(".choose-percent"),
+	yearValue = document.querySelector(".year-value"),
+	monthValue = document.querySelector(".month-value"),
+	dayValue = document.querySelector(".day-value");
 
 let money, time;
 
